f6321e131cb07e38383d2dbdc990947f70d5647a
Chad Jones <chad@crashcode.org>
2021-03-15 22:22:38 +0100

    Initial Commit

---
+++ QuickPass/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QuickPass
+//
+//  Created by Chad Jones on 3/15/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
